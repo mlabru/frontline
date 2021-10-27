3c3dd5b67269e71a9898a20cdca71d5fc00e2187
Milton Abrunhosa <ml_byteman@yahoo.com.br>
2021-10-27 09:23:34 -0300

    Distancia máxima de 50 km para near station.

--- ensemble.sh
+++ ensemble.sh
@@ -2,4 +2,6 @@
 
 echo `date`
 cd /home/webpca/ensemble
-python3.7 /home/webpca/ensemble/frontline.py -t
+
+python3.7 /home/webpca/ensemble/frontline.py > /home/webpca/ensemble/frontline.log 2>&1 &
+python3.7 /home/webpca/ensemble/fronttest.py > /home/webpca/ensemble/fronttest.log 2>&1
